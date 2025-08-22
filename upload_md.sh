f2d7da38d54866ae59b64e904b9b4a58daf10613
ganeshtaru <ganeshtaru91@gmail.com>
2025-08-22 17:02:46 +0530

    data update

--- upload_md.sh
+++ upload_md.sh
@@ -9,8 +9,8 @@ fi
 read -p "Enter IAM username: " iam_user
 
 # This username is hardcoded in sql scripts
-DB_PWD='Tbf7HzaRGj'
-DB_HOST='postgres.sdec.mosip.net'
+DB_PWD='password add'
+DB_HOST='add hostname'
 DB_PORT=5432
 XLS=mosip_master/xlsx
 
