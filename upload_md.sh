1a2507e1748032e876cb9afed57e8d8f3b2733bd
ganeshtaru <ganeshtaru91@gmail.com>
2025-08-22 16:55:37 +0530

    Country-specific data updated

--- upload_md.sh
+++ upload_md.sh
@@ -9,8 +9,8 @@ fi
 read -p "Enter IAM username: " iam_user
 
 # This username is hardcoded in sql scripts
-DB_PWD='1jAR6lVu5u'
-DB_HOST='postgres.stpmosip.gov.st'
+DB_PWD='Tbf7HzaRGj'
+DB_HOST='postgres.sdec.mosip.net'
 DB_PORT=5432
 XLS=mosip_master/xlsx
 
